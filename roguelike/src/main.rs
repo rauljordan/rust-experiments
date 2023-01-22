653531928bc94aec98ef623681dae1efed045e8f
rauljordan <raul@prysmaticlabs.com>
2023-01-22 14:00:23 -0500

    resources

--- roguelike/src/main.rs
+++ roguelike/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use rltk::{GameState, Rltk};
+
+struct State {}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut Rltk) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello Rust World");
+    }
+}
+
+fn main() -> rltk::BError {
+    use rltk::RltkBuilder;
+    let context = RltkBuilder::simple80x50().with_title("Rogue").build()?;
+    let gs = State {};
+    rltk::main_loop(context, gs)
 }
